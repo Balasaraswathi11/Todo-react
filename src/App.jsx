76afff3d8aae4a75ff084c50b90766dd5f959cfb
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-11-24 15:33:14 +0530

    render added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Input from './Components/Input'
 import "./App.css"
-export const Port="http://localhost:5000"
+export const Port="https://todo-backend-6nop.onrender.com"
 const App = () => {
    
   return (<>
